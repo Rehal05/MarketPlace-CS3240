247f68b363825e97c8c4d1dac103a8a6715cf699
lvaschmidt <lvaschmidt@me.com>
2025-10-19 11:12:47 -0400

    update url route to /accounts/moderator instead of /accounts/admin

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", views.signup_view, name="signup"), 
-    path("admin/users/", views.admin_user_list, name="user_list"),
-    path("admin/dashboard/", views.admin_dashboard, name = "dashboard"),
+    path("moderator/users/", views.admin_user_list, name="user_list"),
+    path("moderator/dashboard/", views.admin_dashboard, name = "dashboard"),
 ]
