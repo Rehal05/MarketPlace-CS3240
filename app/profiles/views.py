956d9ec8216c927c8a6d12f2ede7887ccf0b33f3
Navraj Singh <navrajsingh@Navrajs-MacBook-Air.local>
2025-11-13 13:48:16 -0500

    Added ability to display average rating in dashboard banner with username.

--- app/profiles/views.py
+++ app/profiles/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from feedapp.models import Rating
 
 @login_required
 def dashboard(request):
     user = request.user
+    avg_rating = Rating.get_user_average(user)
     context = {
         "username": user.username,
         "signup_date": user.date_joined,
         "profile_image_url": user.profile_pic,
+        "avg_rating": avg_rating
     }
     return render(request, "profiles.html", context)
